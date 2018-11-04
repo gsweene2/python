6a75524ab030fbea27027ddec0da576d4e341485
gsweene2 <gsweene2@u.rochester.edu>
2018-11-03 21:54:05 -0700

    adding cool things

--- learn3.py
+++ learn3.py
@@ -1,1 +1,38 @@
 print("Today I'll be learning python!")
+
+# I can use variables without declaring them
+undeclared = 2018
+print(undeclared)
+
+# I can define strings in multiple ways
+stringVar = 'my string variable'
+stringVar2 = "my other string variable"
+print(stringVar)
+print(stringVar2)
+
+# Holy, you can assign mult vars on one line!
+var1, var2 = "hello", "3"
+print("var1: " + var1)
+print("var2: " + var2)
+
+# You can make a list out of all these vars and iterate through them!
+print()
+print()
+print("Proceeding to print a list")
+newList = []
+newList.append(undeclared)
+newList.append(stringVar)
+newList.append(stringVar2)
+newList.append(var1)
+newList.append(var2)
+
+for x in newList:
+    print(x)
+
+# I can get the length of a list
+lengthList = len(newList)
+print("The length of the list is %s objects" % lengthList)
+
+thisIndex = 2
+thirdObjectLength = len(newList[thisIndex])
+print("The length of the object at index %s is %s characters" % (thisIndex, thirdObjectLength))
